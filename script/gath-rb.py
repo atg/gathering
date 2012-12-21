2d989aa100bafac849471cbfd518435d5c953742
Alex Gordon <anything@fileability.net>
2012-12-21 18:33:44 +0000

    use a single transaction for ruby

--- script/gath-rb.py
+++ script/gath-rb.py
@@ -27,21 +27,20 @@ def addRowRaw(module, parent, name, original_namespace, kind, defline, docs, lin
     canwrite = others['canwrite'] if 'canwrite' in others else ''
     issingleton = others['issingleton'] if 'issingleton' in others else ''
     
-    with db:
-        c = db.cursor()
-        c.execute("""INSERT INTO symbols (
-            namespace, parents, name, original_namespace,
-            type_code, declaration, documentation, sourcedecl,
-            fullsource, superclass,
-            visibility, canread, canwrite, issingleton) VALUES (
-            ?, ?, ?, ?,
-            ?, ?, ?, ?,
-            ?, ?,
-            ?, ?, ?, ?)""", (
-            module, parent, name, original_namespace,
-            kind, defline, removeNonAscii(docs), linedecl,
-            fullsource, superclass,
-            visibility, canread, canwrite, issingleton))
+    c = db.cursor()
+    c.execute("""INSERT INTO symbols (
+        namespace, parents, name, original_namespace,
+        type_code, declaration, documentation, sourcedecl,
+        fullsource, superclass,
+        visibility, canread, canwrite, issingleton) VALUES (
+        ?, ?, ?, ?,
+        ?, ?, ?, ?,
+        ?, ?,
+        ?, ?, ?, ?)""", (
+        module, parent, name, original_namespace,
+        kind, defline, removeNonAscii(docs), linedecl,
+        fullsource, superclass,
+        visibility, canread, canwrite, issingleton))
 
 def splitlinesstrip(s):
     return [line.strip() for line in s.splitlines() if line.strip()]
@@ -91,47 +90,48 @@ def main(argv):
     for record in j:
         if record['type'] == 'namespace':
             modules.append(record['fullname'])
-    for record in j:
-        print record['fullname']
-        if record['type'] == 'method' or record['type'] == 'property':
-            parents, _, name = record['fullname'].rpartition('#')
-        else:
-            parents, _, name = record['fullname'].rpartition('::')
+    with db:
+        for record in j:
+            print record['fullname']
+            if record['type'] == 'method' or record['type'] == 'property':
+                parents, _, name = record['fullname'].rpartition('#')
+            else:
+                parents, _, name = record['fullname'].rpartition('::')
         
-        namespace = ''
-        newparents = parents
-        for mod in modules:
-            if parents.startswith(mod + '::') and len(mod) > len(namespace):
-                namespace = mod
-                newparents = parents[len(mod + '::'):]
-        parents = newparents
+            namespace = ''
+            newparents = parents
+            for mod in modules:
+                if parents.startswith(mod + '::') and len(mod) > len(namespace):
+                    namespace = mod
+                    newparents = parents[len(mod + '::'):]
+            parents = newparents
         
-        print '  ' + str((namespace, parents, name))
-        pprint(record)
+            print '  ' + str((namespace, parents, name))
+            pprint(record)
         
-        if 'fullsignature' in record:
-            fullsignature = record['fullsignature']
-        elif 'signature' in record:
-            fullsignature = record['signature']
-        elif 'minisignature' in record:
-            fullsignature = record['minisignature']
+            if 'fullsignature' in record:
+                fullsignature = record['fullsignature']
+            elif 'signature' in record:
+                fullsignature = record['signature']
+            elif 'minisignature' in record:
+                fullsignature = record['minisignature']
         
-        if 'minisignature' in record:
-            signature = record['minisignature']
-        elif 'signature' in record:
-            signature = record['signature']
-        elif 'fullsignature' in record:
-            signature = record['fullsignature']
+            if 'minisignature' in record:
+                signature = record['minisignature']
+            elif 'signature' in record:
+                signature = record['signature']
+            elif 'fullsignature' in record:
+                signature = record['fullsignature']
         
-        # TODO: CONSTANTS
-        # html = 
+            # TODO: CONSTANTS
+            # html = 
         
-        addRowRaw(namespace, parents, name,
-                  namespace, record['type'],
-                  signature, record['html'], fullsignature
-                  **record)
+            addRowRaw(namespace, parents, name,
+                      namespace, record['type'],
+                      signature, record['html'], fullsignature
+                      **record)
         
-    print modules
+    # print modules
     
 if __name__ == '__main__':
     main(sys.argv)
